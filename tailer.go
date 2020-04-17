272993c0990e43ccbc8cc9b4df826e3b743cae3d
Zoran Rajic <zox@portworx.com>
2020-04-16 22:40:49 -0700

    Initial

---
+++ tailer.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"time"
+)
+
+// FileTailer simple interface to control the tail-follow of the file
+type FileTailer interface {
+	Start() FileTailer
+	Stop()
+	IsRunning() bool
+}
+
+type fileTailer struct {
+	fname     string
+	out       io.Writer
+	isRunning bool
+	ctx       context.Context
+	cancel    context.CancelFunc
+}
+
+// NewFileTailer creates a new instance of the FileTailer
+func NewFileTailer(fname string, out io.Writer, ctx context.Context) FileTailer {
+	if out == nil {
+		out = os.Stderr
+	}
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	ctx2, cancel := context.WithCancel(ctx)
+	return &fileTailer{
+		fname:  fname,
+		out:    out,
+		ctx:    ctx2,
+		cancel: cancel,
+	}
+}
+
+func (f *fileTailer) loop() error {
+	var (
+		fd        *os.File
+		buf       = make([]byte, 2048)
+		bytesRead = int64(0)
+		n         int
+		st        os.FileInfo
+		err       error
+	)
+
+	f.isRunning = true
+	defer func() { f.isRunning = false }()
+
+	for {
+		if st, err = os.Stat(f.fname); os.IsNotExist(err) {
+			logrus.Tracef("fileTailer: File %s does not yet exist", f.fname)
+			goto labSleep
+		} else if fd == nil || err != nil || st.Size() < bytesRead {
+			logrus.Tracef("fileTailer: File recycled -- reopening")
+			bytesRead = 0
+			if fd, err = os.Open(f.fname); err != nil {
+				logrus.WithError(err).Warnf("fileTailer: Error opening file %s", f.fname)
+				return err
+			}
+		}
+
+		if n, err = fd.Read(buf); err == nil {
+			logrus.Tracef("fileTailer: Read %d bytes", n)
+			os.Stdout.Write(buf[0:n])
+			bytesRead += int64(n)
+		} else if err == io.EOF {
+			goto labSleep
+		} else {
+			logrus.WithError(err).Warnf("fileTailer: Error reading file %s", f.fname)
+			return err
+		}
+
+	labSleep:
+		select {
+		case <-time.After(time.Second):
+			// nop
+		case <-f.ctx.Done():
+			logrus.Debugf("fileTailer loop done.")
+			return f.ctx.Err()
+		}
+	}
+}
+
+func (f *fileTailer) Start() FileTailer {
+	go f.loop()
+	return f
+}
+
+func (f *fileTailer) Stop() {
+	f.cancel()
+}
+
+func (f *fileTailer) IsRunning() bool {
+	return f.isRunning
+}
+
+func main() {
+	// ctx, cancel := context.WithCancel(context.Background())
+	// go func() {
+	// 	time.Sleep(10 * time.Second)
+	// 	cancel()
+	// }()
+
+	logrus.SetLevel(logrus.TraceLevel)
+	ft := NewFileTailer("/tmp/tmp.xx", os.Stderr, nil).Start()
+	go func() {
+		time.Sleep(10 * time.Second)
+		ft.Stop()
+	}()
+	time.Sleep(30 * time.Second)
+}
